85778d11fcc91be5005fe6035bda0f700eaa18bc
Danylo Holets <goletsdan@gmail.com>
2023-04-03 14:23:11 +0300

    Added numpy and matrix endpoint

--- Python/spaceship/routers/api.py
+++ Python/spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,17 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_a, matrix_b)
+
+    res = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+    
+    return res
